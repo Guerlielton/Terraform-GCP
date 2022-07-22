3969f695562ba521806bab85327ba8a4b8d6615a
Guerlielton <guerlielton@gmail.com>
2022-07-21 23:05:44 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-module "teste" {
-  source = "git::https://github.com/Guerlielton/modules.git//poc-test/"
+# module "teste" {
+#   source = "git::https://github.com/Guerlielton/modules.git//poc-test/"
+# }
+
+resource "null_resource" "cluster" {
+  provisioner "local-exec" {
+    command = "echo mentoria-iac"
+  }
 }
